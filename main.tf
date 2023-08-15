fd04831571b84b1092a78d2ec17c9aa7f064e4f8
Azeemuddin Mohammed <azeem_parvez@hotmail.com>
2023-08-15 10:16:24 +0000

    main updated with sgs

--- main.tf
+++ main.tf
@@ -2,17 +2,40 @@ module "vpc" {
   source   = "./VPC"
   vpc-cidr = var.vpc-cidr
 }
+
+module "sg-ssh" {
+  source = "./securitygroups"
+  sg_name = "SSH"
+  vpc_id = module.vpc.vpc_id
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_from_port = 22
+  ingress_to_port = 22
+}
+
+module "sg-web" {
+  source = "./securitygroups"
+  sg_name = "HTTP"
+  vpc_id = module.vpc.vpc_id
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_from_port = 8080
+  ingress_to_port = 8080
+}
+
 module "jenkins-instance" {
   source        = "./Jenkins-instances"
+  instance_name = "Jenkins-Master"
   instance_type = var.instance_type
   subnet_id     = module.vpc.public-subnets[0]
+  security_groups = [ module.sg-ssh.security_groups, module.sg-web.security_groups ]
   key_name      = "azeem"
 }
 
 module "jenkins-slaves" {
   source        = "./Jenkins-instances"
+  instance_name = "Jenkins-Slave"
   instance_type = var.instance_type
   subnet_id     = module.vpc.public-subnets[1]
+  security_groups = [ module.sg-ssh.security_groups ]
   key_name      = "azeem"
 }
 
